1bbd7626d2884b2afb5e4d01fc11c5d67c08450d
oguntayo <oguntayohabeebullah@gmail.com>
2025-01-29 13:07:54 +0100

    hng12 stage 0

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from datetime import datetime,timezone
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return{"data":{"name":"bayo"}}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=False, 
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
-@app.get('/about')
-def about():
-    return{"data":"about page"}
+@app.get('/stage_zero')
+def index():
+    #return registered email,current datetime as an ISO format,GitHub url
+    utc_current = datetime.now(timezone.utc)
+    return{
+        "email":"oguntayohabeebullah@gmail.com",
+        "current_datetime":utc_current.strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "github_url":"https://github.com/Oguntayo/hng12-stage-public-api"
+    }
